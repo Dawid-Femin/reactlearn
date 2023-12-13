66f5a9b85a8bbc841785b782837d076699b77081
Dawid Femin <dawid.femin@gmail.com>
2023-12-13 21:48:09 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './styles/reset/reset.scss'
+import './styles/homepage/homepage.scss'
+import Nav from './components/nav/nav.tsx'
+
+function App() {
+
+  return (
+    <>
+      <Nav/>
+    </>
+  )
+}
+
+export default App
